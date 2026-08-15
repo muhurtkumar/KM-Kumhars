c4a8347bee80e2a841a342c6c299f943b455a4e2
namrata <namratajaiswal.716@gmail.com>
2026-08-15 11:31:22 +0530

    How we work (about) added

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,13 @@
 import AboutHero from "../components/About/AboutHero";
 import WhoWeAre from "../components/About/WhoWeAre";
+import HowWeWork from "../components/About/HowWeWork";
 
 export default function About() {
   return (
     <>
       <AboutHero />
       <WhoWeAre />
+      <HowWeWork />
     </>
   );
 }
