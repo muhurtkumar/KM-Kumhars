c7d7270ce10a55a8467ea25bf200a5c0ff72e87f
muhurtkumar <muhurtkumar@gmail.com>
2026-09-13 01:24:50 +0530

    feat: add project schema to Sanity CMS

--- sanity-studio/schemaTypes/index.js
+++ sanity-studio/schemaTypes/index.js
@@ -1,5 +1,7 @@
 import testimonial from "./testimonial";
+import project from "./project";
 
 export const schemaTypes = [
   testimonial,
+  project,
 ];
---
+++ sanity-studio/schemaTypes/project.js
@@ -0,0 +1,51 @@
+export default {
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Project Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'fullCategory',
+      title: 'Full Category Name',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Residential', value: 'Residential' },
+          { title: 'Commercial', value: 'Commercial' },
+          { title: 'Retail & Hospitality', value: 'Retail & Hospitality' },
+          { title: 'Interior Decor', value: 'Interior Decor' },
+          { title: 'Furniture Design', value: 'Furniture Design' }
+        ]
+      }
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+    },
+    {
+      name: 'year',
+      title: 'Completion Year',
+      type: 'string',
+    },
+    {
+      name: 'images',
+      title: 'Project Images',
+      type: 'array',
+      of: [{ type: 'image', options: { hotspot: true } }],
+      description: 'The first image uploaded will be used as the main cover image.',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      subtitle: 'category',
+      media: 'images.0',
+    },
+  },
+};
